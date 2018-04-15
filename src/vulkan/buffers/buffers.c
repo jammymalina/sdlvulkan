7c8376f41ce7b5da4d0b5f17e29135353aaeb11e
jammymalina <jakub.jantosik@gmail.com>
2018-04-15 20:43:06 +0200

    Buffer update

--- src/vulkan/buffers/buffers.c
+++ src/vulkan/buffers/buffers.c
@@ -3,8 +3,215 @@
 #include "../functions/functions.h"
 #include "../tools/tools.h"
 #include "../context.h"
+#include "../memory/staging.h"
 #include "../../utils/heap.h"
 #include "../../logger/logger.h"
 
-GENERATE_BUFFER_FUNCTIONS_SOURCE(vertex_buffer, VK_BUFFER_USAGE_VERTEX_BUFFER_BIT)
+bool copy_buffer_data(byte *dest, const byte *src, VkDeviceSize num_bytes) {
+    if (!is_16_byte_aligned(src) || !is_16_byte_aligned(dest)) {
+        log_error("Copy failed, destination data and src data must be aligned (16 bytes)");
+        return false;
+    }
+    memcpy(dest, src, num_bytes);
+    return true;
+}
 
+static void set_buffer_mapped(vk_buffer *buffer) {
+    buffer->size |= MAPPED_FLAG;
+}
+
+static void set_buffer_unmapped(vk_buffer *buffer) {
+    buffer->size &= ~MAPPED_FLAG;
+}
+
+void init_vk_buffer(vk_buffer *buffer, buffer_type type) {
+    buffer->type = type;
+    buffer->buffer = VK_NULL_HANDLE;
+    buffer->size = 0;
+    buffer->offset_in_other_buffer = OWNS_BUFFER_FLAG;
+    set_buffer_unmapped(buffer);
+    init_vk_allocation(&buffer->allocation);
+}
+
+static VkBufferUsageFlags buffer_type_to_vulkan_buffer_usage(buffer_type type) {
+    switch (type) {
+        case VERTEX_BUFFER:
+            return VK_BUFFER_USAGE_VERTEX_BUFFER_BIT;
+        case INDEX_BUFFER:
+            return VK_BUFFER_USAGE_INDEX_BUFFER_BIT;
+        case UNIFORM_BUFFER:
+            return VK_BUFFER_USAGE_UNIFORM_BUFFER_BIT;
+        default:
+            return 0;
+    }
+}
+
+bool alloc_vk_buffer(vk_buffer *buffer, void *data, VkDeviceSize alloc_size, buffer_usage_type usage) {
+    if (buffer->buffer) {
+        log_error("Buffer already allocated");
+        return false;
+    }
+    if (!is_16_byte_aligned(data)) {
+        log_error("Buffer data is not properly aligned (16 bytes)");
+        return false;
+    }
+    if (alloc_size == 0) {
+        log_error("Cannot allocate buffer of size 0");
+        return false;
+    }
+
+    buffer->size = alloc_size;
+    buffer->usage = usage;
+
+    VkDeviceSize num_bytes = get_allocated_buffer_size(buffer);
+
+    VkBufferCreateInfo buffer_info = {
+        .sType = VK_STRUCTURE_TYPE_BUFFER_CREATE_INFO,
+        .pNext = NULL,
+        .flags = 0,
+        .size = num_bytes,
+        .usage = buffer_type_to_vulkan_buffer_usage(buffer->type),
+        .sharingMode = VK_SHARING_MODE_EXCLUSIVE,
+        .queueFamilyIndexCount = 0,
+        .pQueueFamilyIndices = NULL
+    };
+
+    if (buffer->usage == BU_STATIC) {
+        buffer_info.usage |= VK_BUFFER_USAGE_TRANSFER_DST_BIT;
+    }
+
+    CHECK_VK(vk_CreateBuffer(context.device, &buffer_info, NULL, &buffer->buffer));
+
+    VkMemoryRequirements mem_requirements;
+    vk_GetBufferMemoryRequirements(context.device, buffer->buffer, &mem_requirements);
+
+    vk_memory_usage_type mem_usage = buffer->usage == BU_STATIC ?
+        VULKAN_MEMORY_USAGE_GPU_ONLY : VULKAN_MEMORY_USAGE_CPU_TO_GPU;
+
+    bool success = vk_allocate(&buffer->allocation, mem_requirements.size, mem_requirements.alignment,
+        mem_requirements.memoryTypeBits, mem_usage, VULKAN_ALLOCATION_TYPE_BUFFER);
+
+    if (!success) {
+        return false;
+    }
+
+    CHECK_VK(vk_BindBufferMemory(context.device, buffer->buffer, buffer->allocation.device_memory,
+        buffer->allocation.offset));
+
+    if (data != NULL) {
+        update_data_vk_buffer(buffer, data, alloc_size, 0);
+    }
+
+    return true;
+}
+
+static void clear_buffer(vk_buffer *buffer) {
+    buffer->size = 0; 
+    buffer->offset_in_other_buffer = OWNS_BUFFER_FLAG;
+    buffer->buffer = VK_NULL_HANDLE;
+    buffer->allocation.device_memory = VK_NULL_HANDLE;   
+}
+
+void free_vk_buffer(vk_buffer *buffer) {
+    if (is_buffer_mapped(buffer)) {
+        unmap_vk_buffer(buffer);
+    }
+
+    if (!owns_buffer(buffer)) {
+        clear_buffer(buffer);
+        return;
+    }
+
+    if (!buffer->buffer) {
+        return;
+    }
+
+    vk_free_allocation(&buffer->allocation);
+    vk_DestroyBuffer(context.device, buffer->buffer, NULL);
+    buffer->buffer = VK_NULL_HANDLE;
+    init_vk_allocation(&buffer->allocation);
+
+    clear_buffer(buffer);
+}
+
+bool update_data_vk_buffer(vk_buffer *buffer, void *data, VkDeviceSize size, VkDeviceSize offset) {
+    if (!buffer->buffer) {
+        log_error("Buffer is not allocated (vulkan handle is null)");
+        return false;
+    }
+    if (!is_16_byte_aligned(data)) {
+        log_error("Buffer data is not properly aligned (16 bytes)");
+        return false;
+    }
+    if ((get_buffer_offset(buffer) & 15) != 0) {
+        log_error("Invalid offset");
+        return false;
+    }
+
+    if (buffer->usage == BU_DYNAMIC) {
+        return copy_buffer_data(buffer->allocation.data + get_buffer_offset(buffer) + offset, (const byte*) data, size);
+    }
+
+    VkBuffer stage_buffer;
+    VkCommandBuffer command_buffer;
+    VkDeviceSize stage_offset = 0;
+    byte *stage_data = vk_stage(size, 1, &command_buffer, &stage_buffer, &stage_offset);
+
+    if (!stage_data) {
+        log_error("Could not stage data");
+        return false;
+    }
+
+    memcpy(stage_data, data, size);
+
+    VkBufferCopy buffer_copy = {
+        .srcOffset = stage_offset,
+        .dstOffset = get_buffer_offset(buffer) + offset,
+        .size = size
+    };
+    vk_CmdCopyBuffer(command_buffer, stage_buffer, buffer->buffer, 1, &buffer_copy);
+
+    return true;
+}
+
+bool map_vk_buffer(vk_buffer *buffer, void **dest, buffer_map_type map_type) {
+    if (!buffer->buffer) {
+        log_error("Buffer is not allocated (vulkan handle is null)");        
+        return false;
+    }
+    if (buffer->usage == BU_STATIC) {
+        log_error("Cannot map a static buffer");
+        return false;
+    }
+    if (!buffer->allocation.data) {
+        log_error("Buffer is not allocated (allocation data pointer is null)");
+        return false;
+    }
+
+    void *data = buffer->allocation.data + get_allocated_buffer_size(buffer);
+
+    set_buffer_mapped(buffer);
+
+    if (data == NULL) {
+        log_error("Mapping of data failed");
+    }
+
+    *dest = data;
+
+    return true;
+}
+
+bool unmap_vk_buffer(vk_buffer *buffer) {
+    if (buffer->buffer) {
+        log_error("Buffer already allocated");
+        return false;
+    }
+    if (buffer->usage == BU_STATIC) {
+        log_error("Cannot unmap a static buffer");
+        return false;
+    }
+
+    set_buffer_unmapped(buffer);
+    
+    return true;
+}
--- src/vulkan/buffers/buffers.h
+++ src/vulkan/buffers/buffers.h
@@ -18,118 +18,45 @@ typedef enum buffer_usage_type {
     BU_DYNAMIC
 } buffer_usage_type;
 
-#define BUFFER_BASE                      \
-    VkDeviceSize size;                   \
-    VkDeviceSize offset_in_other_buffer; \
-    buffer_usage_type usage;             \
-    vk_allocation allocation;            \
+typedef enum buffer_type {
+    UNDEFINED_BUFFER,
+    VERTEX_BUFFER,
+    INDEX_BUFFER,
+    UNIFORM_BUFFER
+} buffer_type;
+
+typedef struct vk_buffer {
+    buffer_type type;
+    VkDeviceSize size;
+    VkDeviceSize offset_in_other_buffer;
+    buffer_usage_type usage;
+    vk_allocation allocation;
     VkBuffer buffer;
+} vk_buffer;
 
-typedef struct vertex_buffer {
-    BUFFER_BASE
-} vertex_buffer;
+bool copy_buffer_data(byte *dest, const byte *src, VkDeviceSize num_bytes);
 
-typedef struct index_buffer {
-    BUFFER_BASE
-} index_buffer;
+void init_vk_buffer(vk_buffer *buffer, buffer_type type);
+bool alloc_vk_buffer(vk_buffer *buffer, void *data, VkDeviceSize alloc_size, buffer_usage_type usage);
+void free_vk_buffer(vk_buffer *buffer);
+bool update_data_vk_buffer(vk_buffer *buffer, void *data, VkDeviceSize size, VkDeviceSize offset);
+bool map_vk_buffer(vk_buffer *buffer, void **dest, buffer_map_type map_type);
+bool unmap_vk_buffer(vk_buffer *buffer);
 
-typedef struct uniform_buffer {
-    BUFFER_BASE
-} uniform_buffer;
+static inline VkDeviceSize get_allocated_buffer_size(vk_buffer *buffer) {
+    return ((buffer->size & ~MAPPED_FLAG) + 15) & ~15;
+}
 
-#define init_buffer(buffer)                       \
-        do {                                      \
-            (buffer)->buffer = VK_NULL_HANDLE;    \
-            (buffer)->size = 0;                   \
-            (buffer)->offset_in_other_buffer = 0; \
-            set_buffer_unmapped((buffer));        \
-        } while (0)
+static inline VkDeviceSize get_buffer_offset(vk_buffer *buffer) {
+    return buffer->offset_in_other_buffer & ~OWNS_BUFFER_FLAG;
+}
 
-#define get_allocated_buffer_size(buffer) ((((buffer)->size & ~MAPPED_FLAG) + 15) & ~15)          
+static inline bool owns_buffer(vk_buffer *buffer) {
+    return (buffer->offset_in_other_buffer & OWNS_BUFFER_FLAG) != 0;
+}
 
-#define get_buffer_offset(buffer) ((buffer)->offset_in_other_buffer & ~OWNS_BUFFER_FLAG)
-
-#define is_buffer_mapped(buffer) ((buffer)->size & MAPPED_FLAG != 0)                     
-
-#define set_buffer_mapped(buffer)       \
-    do {                                \
-        (buffer)->size |= MAPPED_FLAG;  \
-    } while(0)
-
-#define set_buffer_unmapped(buffer)     \
-    do {                                \
-        (buffer)->size &= ~MAPPED_FLAG; \
-    } while (0)
-
-#define GENERATE_BUFFER_FUNCTIONS_HEADER(buffer_type)                                                            \
-    bool alloc_##buffer_type(buffer_type *buffer, void *data, VkDeviceSize alloc_size, buffer_usage_type usage); \
-    void free_##buffer_type(buffer_type *buffer);                                                                \
-    bool update_data_##buffer_type(buffer_type *buffer, void *data, VkDeviceSize size, VkDeviceSize offset);     \
-    bool map_##buffer_type(buffer_type *buffer, buffer_map_type);                                                \
-    bool unmap_##buffer_type(buffer_type *buffer);
-
-GENERATE_BUFFER_FUNCTIONS_HEADER(vertex_buffer)
-GENERATE_BUFFER_FUNCTIONS_HEADER(index_buffer)
-GENERATE_BUFFER_FUNCTIONS_HEADER(uniform_buffer)
-
-#define GENERATE_BUFFER_FUNCTIONS_SOURCE(buffer_type, buffer_usage) \
-    bool alloc_##buffer_type(buffer_type *buffer, void *data, VkDeviceSize alloc_size, buffer_usage_type usage) { \
-        if ((buffer)->buffer != VK_NULL_HANDLE) {                                                                 \
-            log_error("Buffer already allocated");                                                                \
-            return false;                                                                                         \
-        }                                                                                                         \
-        if (!is_16_byte_aligned(data)) {                                                                          \
-            log_error("Buffer data is not properly aligned (16 bytes");                                           \
-            return false;                                                                                         \
-        }                                                                                                         \
-        if (alloc_size == 0) {                                                                                    \
-            log_error("Cannot allocate buffer of size 0");                                                        \
-            return false;                                                                                         \
-        }                                                                                                         \
-                                                                                                                  \
-        (buffer)->size = alloc_size;                                                                              \
-        (buffer)->usage = usage;                                                                                  \
-                                                                                                                  \
-        VkDeviceSize num_bytes =  get_allocated_buffer_size((buffer));                                            \
-                                                                                                                  \
-        VkBufferCreateInfo buffer_info = {                                                                        \
-            .sType = VK_STRUCTURE_TYPE_BUFFER_CREATE_INFO,                                                        \
-            .pNext = NULL,                                                                                        \
-            .flags = 0,                                                                                           \
-            .size = num_bytes,                                                                                    \
-            .usage = buffer_usage,                                                                                \
-            .sharingMode = VK_SHARING_MODE_EXCLUSIVE,                                                             \
-            .queueFamilyIndexCount = 0,                                                                           \
-            .pQueueFamilyIndices = NULL                                                                           \
-        };                                                                                                        \
-                                                                                                                  \
-        if ((buffer)->usage == BU_STATIC) {                                                                       \
-            buffer_info.usage |= VK_BUFFER_USAGE_TRANSFER_DST_BIT;                                                \
-        }                                                                                                         \
-                                                                                                                  \
-        CHECK_VK(vk_CreateBuffer(context.device, &buffer_info, NULL, &(buffer)->buffer));                         \
-                                                                                                                  \
-        VkMemoryRequirements mem_requirements;                                                                    \
-        vk_GetBufferMemoryRequirements(context.device, (buffer)->buffer, &mem_requirements);                      \
-                                                                                                                  \
-        vk_memory_usage_type mem_usage = (buffer)->usage == BU_STATIC ?                                           \
-            VULKAN_MEMORY_USAGE_GPU_ONLY : VULKAN_MEMORY_USAGE_CPU_TO_GPU;                                        \
-                                                                                                                  \
-        bool success = vk_allocate(&(buffer)->allocation, mem_requirements.size, mem_requirements.alignment,      \
-            mem_requirements.memoryTypeBits, mem_usage, VULKAN_ALLOCATION_TYPE_BUFFER);                           \
-                                                                                                                  \
-        if (!success) {                                                                                           \
-            return false;                                                                                         \
-        }                                                                                                         \
-                                                                                                                  \
-        CHECK_VK(vk_BindBufferMemory(context.device, (buffer)->buffer, (buffer)->allocation.device_memory,        \
-            (buffer)->allocation.offset));                                                                        \
-                                                                                                                  \
-        if (data != NULL) {                                                                                       \
-                                                                                                                  \
-        }                                                                                                         \
-                                                                                                                  \
-        return true;                                                                                              \
-    }
+static inline bool is_buffer_mapped(vk_buffer *buffer) {
+    return (buffer->size & MAPPED_FLAG) != 0;
+}
 
 #endif // BUFFER_OBJECT_H
