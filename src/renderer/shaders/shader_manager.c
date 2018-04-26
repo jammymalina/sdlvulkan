a4729b53c0275b00672c35c341a77d21d48159f8
jammymalina <jakub.jantosik@gmail.com>
2018-04-26 07:43:05 +0200

    Tiny fix

--- src/renderer/shaders/shader_manager.c
+++ src/renderer/shaders/shader_manager.c
@@ -3,7 +3,6 @@
 #include "../../string/string.h"
 
 void init_shader(shader *s) {
-    string_copy(s->name, MAX_SHADER_NAME_SIZE, "");
     s->module = VK_NULL_HANDLE;
     s->bindings_size = 0;
     s->render_params_size = 0;
