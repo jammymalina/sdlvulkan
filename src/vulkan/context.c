5a994b2bc33c1f4a7d9e1c28112fb814536ebaf0
jammymalina <jakub.jantosik@gmail.com>
2018-05-13 20:05:19 +0200

    Present image optimization

--- src/vulkan/context.c
+++ src/vulkan/context.c
@@ -465,7 +465,7 @@ static bool create_render_pass(vk_context *ctx) {
         .stencilLoadOp  = VK_ATTACHMENT_LOAD_OP_DONT_CARE,
         .stencilStoreOp = VK_ATTACHMENT_STORE_OP_DONT_CARE,
         .initialLayout  = VK_IMAGE_LAYOUT_UNDEFINED,
-        .finalLayout    = VK_IMAGE_LAYOUT_COLOR_ATTACHMENT_OPTIMAL
+        .finalLayout    = VK_IMAGE_LAYOUT_PRESENT_SRC_KHR
     };
 
     VkAttachmentDescription depth_attachment = {
